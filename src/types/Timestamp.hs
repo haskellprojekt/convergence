7568323ecf53604c7a36ec1e63741520819de444
unknown <Tim@Extreme.(none)>
2011-12-18 01:40:40 +0100

    turned Timestamp into module

--- src/types/Timestamp.hs
+++ src/types/Timestamp.hs
@@ -1,4 +1,6 @@
 
+module Timestamp (Timestamp, getStart, getFinish, getDuration, tsHuman, tsJSON) where
+
 -- | 'Timestamp' defines a tuple of start and finish int's
 type Timestamp = (Int, Int)
 
@@ -14,10 +16,10 @@ getFinish (_, y) = y
 getDuration :: Timestamp -> Int
 getDuration (x, y) = y-x
 
--- | 'timestampHuman' returns a string for humans representing the values of the timestamp
-timestampHuman :: Timestamp -> String
-timestampHuman (s, f) = "Timestamp " ++ show s ++ " -> " ++ show f ++ " (" ++ show (f-s) ++ ")"
+-- | 'tsHuman' returns a string for humans representing the values of the timestamp
+tsHuman :: Timestamp -> String
+tsHuman (s, f) = "Timestamp " ++ show s ++ " -> " ++ show f ++ " (" ++ show (f-s) ++ ")"
 
--- | 'timestampJSON' returns a string for JSON representing the values of the timestamp
-timestampJSON :: Timestamp -> String
-timestampJSON (s, f) = "\"timestamp\": {\"start\": \"" ++ show s ++ "\", \"finish\": \"" ++ show f ++ "\"}"
+-- | 'tsJSON' returns a string for JSON representing the values of the timestamp
+tsJSON :: Timestamp -> String
+tsJSON (s, f) = "\"timestamp\": {\"start\": \"" ++ show s ++ "\", \"finish\": \"" ++ show f ++ "\"}"
