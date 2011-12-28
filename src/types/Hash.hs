b9728d9abce6904f32ac1a62fba2533eb6455fde
Leon Sixt <leon@leons-notebook.(none)>
2011-12-28 17:13:07 +0100

    Added the fromX509 function to the Hash module

--- src/types/Hash.hs
+++ src/types/Hash.hs
@@ -1,9 +1,22 @@
 
-module Hash (Hash, hashHuman, hashJSON) where
+module Hash (Hash, fromX509, hashHuman, hashJSON) where
 
 import Numeric
 import Data.Char
 import Data.Word
+import OpenSSL
+import OpenSSL.X509
+import OpenSSL.PEM
+import OpenSSL.EVP.Digest
+import OpenSSL.EVP.Base64
+import Maybe
+import Codec.Binary.Base64.String
+import qualified Codec.Binary.UTF8.String as UTF8
+import Numeric
+import Data.List.Split
+import Data.String.Utils
+import Char
+
 
 -- | 'Hash' defines a List of Word8's
 type Hash = [Word8]
@@ -15,6 +28,23 @@ hashHuman f = "Hash: " ++ hash2String f
 -- | 'hashJSON' generates a JSON representation of the hash's data
 hashJSON :: Hash -> String
 hashJSON f = "\"hash\": \"" ++ hash2String f ++ "\""
+
+fromX509 :: X509 -> IO Hash
+fromX509 x509 = do
+    pem <- writeX509 x509
+    hash <- sha1 $ decode $ cutPem pem
+    return hash
+    where
+        -- cuts "--Begin..." and "---End"
+        cutPem :: String -> String
+        cutPem pem = concat $ init $ tail $ splitOn "\n" pem
+
+sha1 :: String -> IO Hash 
+sha1 str = do
+    dig <- getDigestByName "sha1"
+    let hash = digest (fromJust dig) str
+    return $ UTF8.encode $ hash
+
 
 -- | 'hash2String' generates a String representating the hash's data
 hash2String :: Hash -> String
