200e24afb4729e1ff57cb997a20854a83d374f1f
Leon Sixt <leon@leons-notebook.(none)>
2011-12-28 18:40:27 +0100

    added a fpFromX509 function to the Fingerprint module

--- src/types/Fingerprint.hs
+++ src/types/Fingerprint.hs
@@ -1,16 +1,23 @@
 
-module Fingerprint (Fingerprint, fpHuman, fpJSON) where
+module Fingerprint (Fingerprint, fpFromX509, fpHuman, fpJSON) where
 
 import Timestamp
 import Hash
+import OpenSSL.X509
 
 -- | 'Fingerprint' defines a tupel of timestamp and hash
 type Fingerprint = (Timestamp, Hash)
 
+fpFromX509 :: X509 -> IO Fingerprint
+fpFromX509 x509 = do
+    hash <- hsFromX509 x509
+    timestamp <- tsFromX509 x509
+    return (timestamp, hash)
+
 -- | 'fpHuman' generates a human readable representation of the fingerprint's data
 fpHuman :: Fingerprint -> String
-fpHuman (t, f) = "Fingerprint:\n   " ++ tsHuman t ++ "\n   " ++ fpHuman f ++ "\n"
+fpHuman (t, h) = "Fingerprint:\n   " ++ tsHuman t ++ "\n   " ++ hsHuman h ++ "\n"
 
 -- | 'fpJSON' generates a JSON representation of the fingerprint's data
 fpJSON :: Fingerprint -> String
-fpJSON (t, f) = "{\n   " ++ tsJSON t ++ ",\n   " ++ fpJSON f ++ "\n}"
+fpJSON (t, h) = "{\n   " ++ tsJSON t ++ ",\n   " ++ hsJSON h ++ "\n}"
