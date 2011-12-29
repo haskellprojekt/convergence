a62a6fd0ffc23f262a45c20bc019721e838f3ab4
Leon Sixt <leon@leons-notebook.(none)>
2011-12-29 17:10:06 +0100

    changed the Hash type into a String
    the Hash type changed from [Word8] into String.
    Also hashes are normally a list of Word8s,
    every component handels hashes as string the
    database, the convergence protocoll.

--- src/types/Hash.hs
+++ src/types/Hash.hs
@@ -1,5 +1,8 @@
-
+-- This module was once named Fingerprint,
+-- Hash seems for me more intuitive.
+-- 
 module Hash (Hash, hsFromX509, hsHuman, hsJSON) where
+
 
 import Numeric
 import Data.Char
@@ -18,16 +21,13 @@ import Data.String.Utils
 import Char
 
 
--- | 'Hash' defines a List of Word8's
-type Hash = [Word8]
+-- | 'Hash' defines a Hash String
+type Hash = String
 
--- | 'hsHuman' generates a human readable representation of the hash's data
-hsHuman :: Hash -> String
-hsHuman f = "Hash: " ++ hs2String f
 
 -- | 'hsJSON' generates a JSON representation of the hash's data
 hsJSON :: Hash -> String
-hsJSON f = "\"hash\": \"" ++ hs2String f ++ "\""
+hsJSON hs = "\"hash\": \"" ++ hs ++ "\""
 
 -- | 'hsFromX509' calculates the sha1 hash for a x509 certificate
 hsFromX509 :: X509 -> IO Hash
@@ -40,28 +40,22 @@ hsFromX509 x509 = do
         cutPem :: String -> String
         cutPem pem = concat $ init $ tail $ splitOn "\n" pem
 -- | 'sha1' calculates the sha1 hash
-sha1 :: String -> IO Hash
+sha1 :: String -> IO String
 sha1 str = do
     dig <- getDigestByName "sha1"
     let hash = digest (fromJust dig) str
-    print hash
-    return $ map c2w8 hash
+    return $ concat $ map chr2hex hash
 -- | 'c2w8' translate a character into a word8
-c2w8 :: Char -> Word8
-c2w8 chr = (fromIntegral $ ord chr) :: Word8
-
--- | 'hs2String' generates a String representating the hash's data
-hs2String :: Hash -> String
-hs2String [] = ""
-hs2String (f:fs)
-	| [] == fs  = x
-	| otherwise = x ++ ":" ++ hs2String fs
-	where x = lenTo2 (upperCase (toHex f))
---
+chr2hex :: Char -> String
+chr2hex chr = lenTo2 $ toHex $ ord chr
 
 -- | 'toHex' calculates the hexadecimal representation of a given number
 toHex :: Integral a => a -> String
 toHex n = showIntAtBase 16 intToDigit n ""
+
+
+hsHuman :: String -> String
+hsHuman hs = "Hash: " ++ hs
 
 -- | 'upperCase' converts a String to upper case
 upperCase :: String -> String
