f4dfaa015dc00a09ce99259bd7a4f4a7ed3e0edd
Alexander Sulfrian <alexander@sulfrian.net>
2012-01-03 17:56:28 +0100

    first working web interface try

--- src/convergence.hs
+++ src/convergence.hs
@@ -1,10 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import OpenSSL
-import OpenSSL.EVP.Digest
+import Snap.Core
+import Snap.Util.FileServe
+import Snap.Http.Server
+import qualified Data.ByteString.Char8 as BS
+import Data.String
+import Types.Fingerprint
+import Backend
+import Control.Monad.IO.Class
 
 main :: IO ()
-main = withOpenSSL $
-     do
-     	foo <- getDigestNames
-        print foo
+main = quickHttpServe $
+     route [
+             --("target/:host+:port", method POST doCheck),
+             ("target/:host", method GET doQuery)
+           ]
+
+getRequest :: String -> (String, Int)
+getRequest t = (getHost t, read $ getPort t)
+           where
+                getHost = takeWhile (/= ' ')
+                getPort = dropWhile (/= ' ') . drop 1
+
+doQuery :: Snap ()
+doQuery = do
+        param <- getParam "host"
+        --maybe (writeBS "must specify host param in URL")
+        let (host, port) = Main.getRequest $ maybe "" BS.unpack param
+        do
+                fp <- liftIO $ queryFingerprint host port
+                writeBS $ BS.pack $ fpJSON fp
+
