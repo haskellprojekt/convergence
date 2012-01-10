474440454ab68d280660ab6422e28959b6c01b85
Alexander Sulfrian <alexander@sulfrian.net>
2012-01-10 18:18:33 +0100

    main: added database to request

--- src/convergence.hs
+++ src/convergence.hs
@@ -8,13 +8,16 @@ import Data.String
 import Types.Fingerprint
 import Backend
 import Control.Monad.IO.Class
+import Database
+import Database.SQLite
 
 main :: IO ()
-main = quickHttpServe $
-     route [
-             --("target/:host+:port", method POST doCheck),
-             ("target/:host", method GET doQuery)
-           ]
+main = do
+     db <- Database.connect
+     quickHttpServe $ route [
+         --("target/:host+:port", method POST doCheck),
+         ("target/:host", method GET (doQuery db))
+     ]
 
 getRequest :: String -> (String, Int)
 getRequest t = (getHost t, read $ getPort t)
@@ -22,12 +25,11 @@ getRequest t = (getHost t, read $ getPort t)
                 getHost = takeWhile (/= ' ')
                 getPort = dropWhile (/= ' ') . drop 1
 
-doQuery :: Snap ()
-doQuery = do
+doQuery :: SQLiteHandle -> Snap ()
+doQuery db = do
         param <- getParam "host"
         --maybe (writeBS "must specify host param in URL")
         let (host, port) = Main.getRequest $ maybe "" BS.unpack param
-        do
-                fp <- liftIO $ queryFingerprint host port
-                writeBS $ BS.pack $ fpJSON fp
+        fp <- liftIO $ getFingerprints db host port
+        writeBS $ BS.pack $ concat $ map fpJSON fp
 
