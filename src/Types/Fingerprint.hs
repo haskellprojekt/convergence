e3372c0ffced564d9c220582299e3e4d606d1c8e
Leon Sixt <leon@leons-notebook.(none)>
2012-01-12 21:11:38 +0100

    fingerprint: added fpsJSON
    fpsJSON converts a list of fingerprints into the json format.
    the fingerprints are signed with the private key.

--- src/Types/Fingerprint.hs
+++ src/Types/Fingerprint.hs
@@ -3,6 +3,10 @@ module Types.Fingerprint (Fingerprint, fpFromX509, fpHuman, fpJSON) where
 import Types.Timestamp
 import Types.Hash
 import OpenSSL.X509
+import OpenSSL.EVP.Digest
+import OpenSSL.EVP.PKey
+import OpenSSL.EVP.Sign
+import Data.List.Utils
 
 -- | 'Fingerprint' defines a tupel of timestamp and hash
 type Fingerprint = (Timestamp, Hash)
@@ -20,3 +24,13 @@ fpHuman (t, h) = "Fingerprint:\n   " ++ tsHuman t ++ "\n   " ++ hsHuman h ++ "\n
 -- | 'fpJSON' generates a JSON representation of the fingerprint's data
 fpJSON :: Fingerprint -> String
 fpJSON (t, h) = "{\n   " ++ tsJSON t ++ ",\n   " ++ hsJSON h ++ "\n}"
+
+fpsJSON :: KeyPair key => key -> [Fingerprint] -> IO String
+fpsJSON key fps = do
+    let jsonstr = "{\n    fingerprintList: [ " 
+                    ++ (join ",\n" $ map fpJSON fps) ++ "]"
+    let str2sign = (replace "\n" "" $ replace " " "" jsonstr) ++ "}"
+    (Just dgst) <- getDigestByName "sha1"
+
+    signature <- sign dgst key str2sign
+    return $ jsonstr ++ ", signature: \"" ++ signature ++ "\"\n}"
