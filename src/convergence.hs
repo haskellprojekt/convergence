06f614314a4bc44153da96b2b95c4e611590c859
Leon Sixt <leon@leons-notebook.(none)>
2012-01-12 21:13:38 +0100

    main: added SSL support to the server

--- src/convergence.hs
+++ src/convergence.hs
@@ -3,6 +3,7 @@ module Main where
 import Snap.Core
 import Snap.Util.FileServe
 import Snap.Http.Server
+import Snap.Http.Server.Config
 import qualified Data.ByteString.Char8 as BS
 import Data.String
 import Types.Fingerprint
@@ -10,14 +11,25 @@ import Backend
 import Control.Monad.IO.Class
 import Database
 import Database.SQLite
+import qualified OpenSSL.Session as SSL
+
+certificateFile = "cert.pem"
+privateKeyFile = "private.pem"
 
 main :: IO ()
-main = do
-     db <- Database.connect
-     quickHttpServe $ route [
-         --("target/:host+:port", method POST doCheck),
-         ("target/:host", method GET (doQuery db))
-     ]
+main = start
+
+start :: IO ()
+start = do
+    db <- Database.connect
+    httpServe serverConfig $ route [
+        --("target/:host+:port", method POST doCheck),
+        (BS.pack "target/:host", method GET (doQuery db))]
+
+serverConfig :: MonadSnap m => Config m a
+serverConfig = setSSLKey privateKeyFile $
+             setSSLCert certificateFile $
+             setSSLPort 8002 $ defaultConfig
 
 getRequest :: String -> (String, Int)
 getRequest t = (getHost t, read $ getPort t)
@@ -27,7 +39,7 @@ getRequest t = (getHost t, read $ getPort t)
 
 doQuery :: SQLiteHandle -> Snap ()
 doQuery db = do
-        param <- getParam "host"
+        param <- getParam $ BS.pack "host"
         --maybe (writeBS "must specify host param in URL")
         let (host, port) = Main.getRequest $ maybe "" BS.unpack param
         fp <- liftIO $ getFingerprints db host port
