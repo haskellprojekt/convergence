931198aca4ef47f256f4fa022267559889460ca9
Leon Sixt <leon@leons-notebook.(none)>
2012-01-24 19:15:28 +0100

    changed SSL Port from Nothing to 8443

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,8 @@ module Main (main) where
 import CVG.Main
 import CVG.Config
 
+
 main :: IO ()
 main = CVG.Main.start defaultConfig {
-     ports = Ports { plain_port = Just 8080, ssl_port = Nothing }
+     ports = Ports { plain_port = Just 8080, ssl_port = Just 8443}
 }
